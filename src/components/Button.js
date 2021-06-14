2dcc5e89aea7533a59c3461bd20ebf6784254ff5
kamleshp95 <kamleshpadwal@gmail.com>
2021-06-14 20:07:44 +0530

    Change button styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,17 +4,18 @@ import styled from 'styled-components';
 
 const ButtonStyle = styled.div`
     margin-top: 2rem;
-    .button{
+    .button {
+        font-size: 2.2rem;
         background-color: ${(props) =>
             props.outline ? 'transparent' : 'var(--gray-1)'};
-        padding: 0.7rem 2rem;
+        padding: 0.7em 2em;
         border-radius: 8px;
         display: inline-block;
         border: 2px solid var(--gray-1);
         color: ${(props) => (props.outline ? 'var(--gray-1)' : 'black')};
     }
     @media only screen and (max-width: 768px){
-        .button{
+        .button {
             font-size: 1.8rem;
         }
     }
